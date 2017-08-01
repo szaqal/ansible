ea76e8c87dcbd748aca2d41b97045ef08da31899
szaqal <malczyk.pawel@gmail.com>
2017-08-01 17:52:43 +0200

    RHEL6 DB

---
+++ virsh/create_db_vm_rhel6.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source commons.sh
+
+set -x
+
+function create_vm {
+virt-install \
+--virt-type kvm \
+--name $1 \
+--ram 8192 \
+--vcpus 2 \
+--network bridge=br0 \
+--cdrom ${VM_CENTOS_6_INSTALLATION_IMAGE} \
+--disk path=${GM_PATH}/$1,size=100  \
+--graphics vnc \
+--os-type linux \
+--os-variant rhel6 
+}
+
+echo "Enter vm name";
+
+read VM_NAME
