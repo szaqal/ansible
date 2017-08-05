1bbf6d9ee52a90695ce8ec1adec01a0bab4d7a8b
szaqal <malczyk.pawel@gmail.com>
2017-08-05 09:51:01 +0200

    snapshot name

--- virsh/create_vm_snapshot.sh
+++ virsh/create_vm_snapshot.sh
@@ -4,11 +4,16 @@ source commons.sh
 
 
 function create_snapshot {
-  virsh snapshot-create  $1
+  virsh snapshot-create-as --domain  $1 --name "$2"
 }
 
 echo "Enter VM name"
 
 read VM_NAME
 
-create_snapshot $VM_NAME
+echo "Enter snapshot name"
+
+read SNAP_NAME
+
+create_snapshot $VM_NAME $SNAP_NAME
+
